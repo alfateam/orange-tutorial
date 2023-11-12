3aee60b0ac09704165b6a9f744f3f79c11d36cb2
Lars-Erik Roald <lars.roald@systor.st>
2023-11-12 13:18:23 +0000

    insert customers

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+import rdb from 'rdb';
 import map from './map.js';
 import init from './init.js';
+
 const db = map.mssql('Server=mssql;Database=master;uid=sa;pwd=P@assword123;TrustServerCertificate=yes;Trusted_Connection=No');
 
 await init(db);
 
+rdb.on('query', console.dir);
+
+const harry = await db.customer.insert({
+    name: 'Harry'
+});
+
+const wolfgang = await db.customer.insert({
+    name: 'Wolgang'
+});
+
+console.dir(wolfgang)
+console.dir(harry);
