036a2e6500777b3c08980d3283f62a057f5d0130
Lars-Erik Roald <lars.roald@systor.st>
2023-11-12 11:35:04 +0000

    v0_initial_commit

---
+++ init.js
@@ -0,0 +1,39 @@
+const sql = `
+DROP TABLE if exists orderLine;
+DROP TABLE if exists deliveryAddress;
+DROP TABLE if exists _order;
+DROP TABLE if exists customer;
+
+CREATE TABLE customer (
+    id int IDENTITY(1,1) PRIMARY KEY,   
+    name VARCHAR(100),
+    balance NUMERIC,
+    isActive BIT
+);
+
+CREATE TABLE _order (
+    id int IDENTITY(1,1) PRIMARY KEY,
+    orderDate DATETIME,
+    customerId INTEGER REFERENCES customer
+);
+
+CREATE TABLE orderLine (
+    id int IDENTITY(1,1) PRIMARY KEY,
+    orderId INTEGER REFERENCES _order,
+    product VARCHAR(100)
+);
+
+CREATE TABLE deliveryAddress (
+    id int IDENTITY(1,1) PRIMARY KEY,
+    orderId INTEGER REFERENCES _order,
+    name VARCHAR(100), 
+    postalPlace VARCHAR(100)
+)
+`;
+
+module.exports = async function(db) {
+	const statements = sql.split(';');
+	for (let i = 0; i < statements.length; i++) {
+		await db.query(statements[i]);
+	}
+};
