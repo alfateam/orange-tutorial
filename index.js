36535ad9725117232809f5499f8ab73dde2be5f0
Lars-Erik Roald <lars.roald@systor.st>
2024-06-16 11:07:32 +0000

    amount sql

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import map from './map.js';
 import init from './init.js';
-const db = map.mssql('Server=mssql;Database=master;uid=sa;pwd=P@assword123;TrustServerCertificate=yes;Trusted_Connection=No');
+import db from './db.js';
 
 await init(db);
-
---
+++ map.js
@@ -0,0 +1,5 @@
+import orange from 'orange-orm';
+
+export default orange.map(() => {
+    return {};
+});
